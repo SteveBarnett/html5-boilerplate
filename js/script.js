7ba5f9b3e66f1249ef585d960cf7280669c506b1
Steve Barnett <steve@naga.co.za>
2012-08-24 15:11:49 +0200

    Add SASS, js scaffold

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 /* Author:
-
+Steve Barnett | Naga | naga.co.za
 */
 
+// (function(win) {
 
+// 		Modernizr.load({
 
+// 			test: Modernizr.XXX,
 
+// 			yep: {
+// 				'XXX' : '/js/.js'
+// 			},
 
+// 			nope: {
+// 				'YYY' : 'js/.js'
+// 			},
+
+// 			callback: {
+
+// 			    'XXX' : function() {
+
+// 			    },
+
+// 				"YYY": function () {
+
+// 			    }
+
+// 			}
+
+// 		})
+
+// })(this)
