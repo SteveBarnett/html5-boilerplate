069aeea2f1957fb499c9dc82bef37d09d9bd60b4
Steve Barnett <steve@naga.co.za>
2013-03-10 19:06:44 +0200

    Add modernBrowser check

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+// "Modern enough" snippet
+var modernBrowser = function () {
+  if('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
+    return true;
+  }
+  return false;
+};
