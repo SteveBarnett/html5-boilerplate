c67e17f65990d761b7f025b47cf6412e7c7f732d
Steve Barnett <steve@naga.co.za>
2013-03-11 16:21:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 // "Modern enough" snippet
+// http://caniuse.com/queryselector
+// http://caniuse.com/namevalue-storage
+
 var modernBrowser = function () {
   if('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
     return true;
