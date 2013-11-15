7c6995768ec9e5fe3d1f2c08dccf027ceb2965c0
Steve Barnett <steve@naga.co.za>
2013-11-15 16:05:50 +0200

    Added performance.timing reminder

--- js/main.js
+++ js/main.js
@@ -8,3 +8,12 @@ var modernBrowser = function () {
   }
   return false;
 };
+
+// Consider using load time for best guess about connection
+// https://developer.mozilla.org/en-US/docs/Navigation_timing
+
+function logPerceivedLoadTime() {
+  var now = new Date().getTime();
+  var page_load_time = now - performance.timing.navigationStart;
+  console.log("User-perceived page loading time: " + page_load_time);
+}
