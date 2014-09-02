efd8b5fca2521962b5f5a8986675737b55dc93b2
Steve Barnett <steve@naga.co.za>
2014-09-02 08:40:51 +0200

    Add requestAnimationFrame

--- js/main.js
+++ js/main.js
@@ -80,3 +80,9 @@ window.addEventListener('error', function(e) {
 //         true
 //     ]);
 // });
+
+// var requestAnimationFrame = window.requestAnimationFrame
+//     || window.webkitRequestAnimationFrame
+//     || window.mozRequestAnimationFrame
+//     || window.msRequestAnimationFrame
+//     || function(callback) { return setTimeout(callback, 1000 / 60); };
